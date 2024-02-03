38828e91a01a2bbeef192002e41f44e5fdb883af
unknown <aguit.inan@gmail.com>
2024-02-03 21:30:18 +0100

    feat: update Exercice02HotelAgain

---
+++ Exercice02HotelAgain/Models/Hotel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice02HotelAgain.Models
+{
+    internal class Hotel
+    {
+        // Propriétés
+        public int HotelId { get; set; }
+        public string Nom { get; set; }
+
+        // Relations
+        public List<Client> Clients { get; set; } = new List<Client>();
+        public List<Chambre> Chambres { get; set; } = new List<Chambre>();
+        public List<Reservation> Reservations { get; set; } = new List<Reservation>();
+
+
+
+
+
+
+
+    }
+}
