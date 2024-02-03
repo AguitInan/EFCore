e2757ae1e2f290b9312aad3dd2d9c2bf0da37798
unknown <aguit.inan@gmail.com>
2024-02-03 21:31:21 +0100

    feat: update Exercice02HotelAgain

---
+++ Exercice02HotelAgain/Models/Reservation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice02HotelAgain.Models
+{
+
+    internal class Reservation
+    {
+        // Propriétés
+        //public int Identifiant { get; set; }
+        //public StatutReservation Statut { get; set; }
+        //public List<Chambre> Chambres { get; set; }
+        //public Client Client { get; set; }
+
+        public int ReservationId { get; set; }
+        public StatutReservation Statut { get; set; }
+
+        // Clé étrangère pour Client
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+
+        // Clé étrangère pour Hotel
+        public int HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+
+
+
+
+    }
+}
