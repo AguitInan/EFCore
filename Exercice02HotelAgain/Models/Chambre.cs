914c43b9b5d0921e834011a21c330d8bffc5ec3b
unknown <aguit.inan@gmail.com>
2024-02-03 21:27:18 +0100

    feat: add Exercice02HotelAgain

---
+++ Exercice02HotelAgain/Models/Chambre.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice02HotelAgain.Models
+{
+
+    internal class Chambre
+    {
+        // Propriétés
+        public int ChambreId { get; set; }
+        public StatutChambre Statut { get; set; }
+        public int NombreDeLits { get; set; }
+        public decimal Tarif { get; set; }
+
+        // Clé étrangère pour Hotel
+        public int HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+
+ 
+
+
+
+    }
+}
