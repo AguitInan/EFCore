0939363bb7b5941ffde1be451cd9bfd0d8907fe7
unknown <aguit.inan@gmail.com>
2024-02-03 21:29:09 +0100

    feat: update Exercice02HotelAgain

---
+++ Exercice02HotelAgain/Models/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice02HotelAgain.Models
+{
+    internal class Client
+    {
+        // Propriétés
+        public int ClientId { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string NumeroTelephone { get; set; }
+
+        // Relation: Un client peut avoir plusieurs réservations
+        public List<Reservation> Reservations { get; set; } = new List<Reservation>();
+
+
+
+
+
+    }
+}
