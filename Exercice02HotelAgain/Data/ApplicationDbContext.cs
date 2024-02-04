74f2a6ec15f7328b6749b83688adc6a411c000c3
unknown <aguit.inan@gmail.com>
2024-02-04 16:36:46 +0100

    feat: update Exercice02HotelAgain

---
+++ Exercice02HotelAgain/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Exercice02HotelAgain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice02HotelAgain.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Chambre> Chambres { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+
+        public ApplicationDbContext() : base()
+        {
+        }
+
+
+
+
+
+
+    }
+}
