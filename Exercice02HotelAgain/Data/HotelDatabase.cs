2608ed71000379eb0f7021d8598367879ecce71b
unknown <aguit.inan@gmail.com>
2024-02-04 16:38:59 +0100

    feat: update Exercice02HotelAgain

---
+++ Exercice02HotelAgain/Data/HotelDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Exercice02HotelAgain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Exercice02HotelAgain.Models.Reservation;
+
+namespace Exercice02HotelAgain.Data
+{
+    public class HotelDatabase
+    {
+        private ApplicationDbContext context;
+
+        public HotelDatabase()
+        {
+            context = new ApplicationDbContext();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
