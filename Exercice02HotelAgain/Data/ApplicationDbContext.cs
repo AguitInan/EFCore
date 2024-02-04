d71530e7be3ec75003de90259c622ed6abd370f0
unknown <aguit.inan@gmail.com>
2024-02-04 16:37:25 +0100

    feat: update Exercice02HotelAgain

--- Exercice02HotelAgain/Data/ApplicationDbContext.cs
+++ Exercice02HotelAgain/Data/ApplicationDbContext.cs
@@ -26,7 +26,46 @@ namespace Exercice02HotelAgain.Data
             optionsBuilder.UseSqlServer("Data source=(localdb)\\demo01ado; Database=HotelAgain;");
         }
 
+        // S'exécute lors de la création des tables
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
 
+            // Configuration de la relation plusieurs-à-plusieurs entre Chambre et Reservation
+            modelBuilder.Entity<Reservation>()
+                .HasMany(r => r.Chambres)
+                .WithMany(c => c.Reservations)
+                .UsingEntity<Dictionary<string, object>>(
+                    "ReservationChambre", // Nom de la table de jonction
+                    j => j.HasOne<Chambre>().WithMany().HasForeignKey("ChambreNumero").OnDelete(DeleteBehavior.NoAction), // Configurer la relation et la suppression pour Chambre
+                    j => j.HasOne<Reservation>().WithMany().HasForeignKey("ReservationIdentifiant").OnDelete(DeleteBehavior.NoAction) // Configurer la relation et la suppression pour Reservation
+                );
+
+            // ... autres configurations
+
+            base.OnModelCreating(modelBuilder);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
 
 
     }
