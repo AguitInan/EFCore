bbfde3b7084bcf5f10894f7ce413789621abaef3
unknown <aguit.inan@gmail.com>
2024-02-04 16:41:33 +0100

    feat: update Exercice02HotelAgain

---
+++ Exercice02HotelAgain/Classes/IHM.cs
@@ -0,0 +1,23 @@
+﻿using Exercice02HotelAgain.Data;
+using Exercice02HotelAgain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Exercice02HotelAgain.Models.Reservation;
+
+namespace Exercice02HotelAgain
+{
+    internal class IHM
+    {
+        private HotelDatabase database;
+
+        public IHM()
+        {
+            database = new HotelDatabase();
+        }
+
+
+    }
+}
