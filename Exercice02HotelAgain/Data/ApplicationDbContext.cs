c66ccdc2485fcf0348255c062f1d6f4722c0886a
unknown <aguit.inan@gmail.com>
2024-02-04 16:38:10 +0100

    feat: update Exercice02HotelAgain

--- Exercice02HotelAgain/Data/ApplicationDbContext.cs
+++ Exercice02HotelAgain/Data/ApplicationDbContext.cs
@@ -96,7 +96,20 @@ namespace Exercice02HotelAgain.Data
 
 
 
+            //modelBuilder.Entity<PostTag>()
+            //    .HasKey(pt => new { pt.TagId, pt.PostId });
 
+            //// HasData permet d'ajouter des données dans la base lors de la création des tables
+            //modelBuilder.Entity<Blog>().HasData(
+            //    new Blog() { Id = 1, Nom = "JohnnyHalliday115", Url = "http://www.johnnyhalliday115.skyblog.com" },
+            //new Blog() { Id = 2, Nom = "Koreus", Url = "http://www.koreus.com" },
+            //    new Blog() { Id = 3, Nom = "Toto", Url = "http://www.toto.com" });
+
+            //modelBuilder.Entity<Post>().HasData(
+            //    new Post() { Id = 1, Titre = "Johnny est mort.", BlogId = 1, Contenu = "Trop triste :'(" },
+            //    new Post() { Id = 2, Titre = "Trop mdr xd la vidéo OLALALA", BlogId = 2, Contenu = "Le chat il tombe là" },
+            //    new Post() { Id = 3, Titre = "Tu connais la blague de toto aux toilettes ?", BlogId = 3, Contenu = "Moi non plus la porte était fermée ^^" }
+            //    );
         }
 
 
